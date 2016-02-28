722705125e04da0c035635cb2dcdbd1de9378e6d
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-02-28 17:17:48 +0200

    add simple middleware

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import reducer from './reducers'
+import logger from './middlewares/logger'
 
-const store = createStore(reducer)
+const store = applyMiddleware(logger)(createStore)(reducer)
 
 window.store = store
 export default store
